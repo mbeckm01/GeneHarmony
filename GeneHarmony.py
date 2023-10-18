608026a9ab78369b9961243d274ee88a9398a4fc
asdalexander <aalexa78@outlook.com>
2023-10-18 08:44:07 -0400

    Big changes to filtering logic

--- GeneHarmony.py
+++ GeneHarmony.py
@@ -5,6 +5,9 @@ import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 
+st.set_page_config(
+    layout="wide"
+)
 
 # Function to connect to the database
 def connect_to_database():
@@ -29,23 +32,46 @@ def get_disease_names(conn):
         return disease_names
 
 # Function to execute a query to retrieve gene names for selected disease names
-def get_gene_names_for_diseases(conn, disease_group):
-        cursor = conn.cursor()
-        # SQL query to fetch distinct gene names for the given diseases
-        query = "SELECT DISTINCT gene_name FROM gene_disease WHERE disease_name IN %s;"
-        cursor.execute(query, (tuple(disease_group),))
-        gene_names = [row[0] for row in cursor.fetchall()]
-        return gene_names
+def get_gene_names_for_diseases(conn, disease_group, min_confidence, max_confidence, selected_sources):
+    cursor = conn.cursor()
+    query = """
+        SELECT DISTINCT gd.gene_name, gd.disease_name 
+        FROM gene_disease gd
+        INNER JOIN diseases_full df ON gd.gene_name = df.gene_name
+        WHERE gd.disease_name IN %s AND df.confidence_score BETWEEN %s AND %s
+    """
 
-# Function to load gene expression data from a .tsv file
-def load_gene_expression_data(file_path):
-        gene_expression_df = pd.read_csv(file_path, sep="\t")
-        return gene_expression_df
+    params = [tuple(disease_group), min_confidence, max_confidence]
+
+    # Add the source_clean condition if selected_sources is not empty
+    if selected_sources:
+        query += " AND df.source_clean IN %s"
+        params.append(tuple(selected_sources))
+
+    cursor.execute(query, params)
+    gene_disease_pairs = cursor.fetchall()
+    cursor.close()
+    return gene_disease_pairs
+
+
+# Load gene expression data from .tsv file
+gene_expression_df = pd.read_csv('/home/adam/PycharmProjects/Gene-Disease-DB/gtex_output.tsv', sep="\t")
 
 # Function to get gene expression values for common genes
 def get_gene_expression_values(gene_expression_df, common_genes):
         gene_expression_subset = gene_expression_df[gene_expression_df['Description'].isin(common_genes)]
         return gene_expression_subset
+
+def get_source_clean_values(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT DISTINCT source_clean FROM diseases_full;")
+    values = [row[0] for row in cursor.fetchall()]
+    cursor.close()
+    return values
+
+
+
+
 
 conn = connect_to_database()
 st.title("GeneHarmony")
@@ -60,6 +86,8 @@ if 'num_groups' not in st.session_state:
     st.session_state['selections'] = {}
     st.session_state['search_results'] = None
     st.session_state['temp_selections'] = {}
+    st.session_state.setdefault('searched', False)
+
 
 # Add Group button
 if st.button("Add Group"):
@@ -68,6 +96,26 @@ if st.button("Add Group"):
 # Remove Group button
 if st.button("Remove Group") and st.session_state['num_groups'] > 1:
     st.session_state['num_groups'] -= 1
+
+# Create columns to control the layout
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col1:  # Use the middle column which has more space allocated to it
+    min_confidence, max_confidence = st.slider(
+        "Select a range for the confidence score",
+        min_value=0.0,
+        max_value=5.0,
+        value=(0.0, 5.0),
+        step=0.1
+    )
+
+# Fetch source_clean values
+source_clean_values = get_source_clean_values(conn)
+
+selected_source_clean = st.multiselect(
+    "Select Source Type",
+    source_clean_values
+)
 
 # Set the number of groups
 num_groups = st.session_state['num_groups']
@@ -79,31 +127,53 @@ for i in range(num_groups):
     selected_names = st.multiselect(f"Select Disease Names - Group {i+1}", disease_names, key=key, default=st.session_state['selections'][key])
     st.session_state['temp_selections'][key] = selected_names
 
+
+# Function to consolidate genes for each disease
+def consolidate_gene_data(gene_disease_pairs):
+    # Convert pairs into a dictionary: {disease_name: [gene1, gene2,...]}
+    consolidated_data = {}
+    for gene, disease in gene_disease_pairs:
+        if disease in consolidated_data:
+            consolidated_data[disease].append(gene)
+        else:
+            consolidated_data[disease] = [gene]
+
+    # Convert the dictionary into a dataframe with additional gene counts
+    df_list = [
+        {
+            "Disease": disease,
+            "Genes": ", ".join(genes),
+            "Gene Count": len(genes)
+        }
+        for disease, genes in consolidated_data.items()
+    ]
+    consolidated_df = pd.DataFrame(df_list)
+
+    return consolidated_df
+
+
 # Search button (to perform search)
 if st.button("Search"):
     st.session_state['selections'] = st.session_state['temp_selections']
 
-    # Execute queries and perform self-join
-    search_results = None
+    # For consolidating gene-disease results
+    all_gene_disease_pairs = []
+
     if conn is not None:
         for key, selected_names in st.session_state['selections'].items():
             if selected_names:
-                gene_names = get_gene_names_for_diseases(conn, selected_names)
-                if search_results is None:
-                    search_results = set(gene_names)
-                else:
-                    search_results = search_results.intersection(gene_names)
+                gene_disease_pairs = get_gene_names_for_diseases(conn, selected_names, min_confidence, max_confidence, selected_source_clean)
+                all_gene_disease_pairs.extend(gene_disease_pairs)
         conn.close()
 
-    if search_results:
-        search_results = pd.DataFrame(list(search_results), columns=["Common Gene Names"])
-    else:
-        search_results = pd.DataFrame(columns=["Common Gene Names"])
+    consolidated_df = consolidate_gene_data(all_gene_disease_pairs)
+    st.dataframe(consolidated_df)
 
-    st.session_state['search_results'] = search_results
+    # For the heatmap, we only want to display common genes
+    # Extracting only the gene names
+    common_genes = [gene for gene, _ in all_gene_disease_pairs]
+    st.session_state['search_results'] = pd.DataFrame(common_genes, columns=["Common Gene Names"])
 
-# Load gene expression data
-gene_expression_df = load_gene_expression_data('/home/adam/PycharmProjects/Gene-Disease-DB/gtex_output.tsv')
 
 if gene_expression_df is not None:
     # Display gene expression values for common genes as a heatmap
@@ -112,6 +182,9 @@ if gene_expression_df is not None:
         gene_expression_subset = get_gene_expression_values(gene_expression_df, common_genes)
 
         if gene_expression_subset is not None:
+            # Display gene names in the heatmap
+            st.dataframe(pd.DataFrame(common_genes, columns=["Gene Names in Heatmap"]))
+
             # Create a long-format DataFrame for Plotly heatmap
             gene_expression_long = gene_expression_subset.melt(id_vars=["Description"], var_name="Tissue", value_name="Expression")
 
--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # Gene-Disease-DB
 
-# What is it
+### Methods (Abstract Oct '23)
+Data infrastructure was established on a Postgres database using Plotly v5.17.0 and Streamlit v1.27.2 Python v3.8 packages. The Diseases v2.0 and GTEx v8 databases were utilized, providing searchable disease names, affiliated gene symbols, confidence scores (ranging from 0 to 5 with 5 being the most confident), and gene expression across 54 tissue types. The microbe-set enrichment analysis database provided context about bacteria possibly affecting expression of genes.
 
+## Explaining session state in Streamlit
+Streamlit's session state allows retaining values across reruns of the app, effectively allowing us to keep track of user interactions, selections, and any other relevant data.
 
+We are checking if the 'num_groups' key is not already present in the session state.
+This check is important because, on the first run of the app, this key won't exist. However, on subsequent runs, we don't want to overwrite the existing value. Also note, refreshing the webpage will reset the session state.
+<br>
+`if 'num_groups' not in st.session_state:`
+    
+'num_groups' key will store the number of disease groups the user wants to search for.
+Initially, we set it to 1, meaning by default, the user can search for genes associated with one group of diseases.
+<br>
+`st.session_state['num_groups'] = 1`
+    
+'selections' key is a dictionary that will keep track of the user's disease name selections for each disease group. For instance, if the user selects "Cancer" and "Flu" for Group 1, and "Diabetes" for Group 2, this dictionary will look like: 
+<br> **{'group_0': ['Cancer', 'Flu'], 'group_1': ['Diabetes']}**
+<br> 
+`st.session_state['selections'] = {}`
 
-# How does it work
-The user interface is served by Streamlit.
-
-Plots are served by Plotly.
-
-
-
-
-
-
+'search_results' key will store the results after a search is performed. Initially, we set it to None because no search has been performed yet.
+<br>
+`st.session_state['search_results'] = None`
+    
+'temp_selections' is similar to 'selections', but it temporarily holds the selections made by the user. This allows us to track the user's current choices without necessarily committing them to the main 'selections' dictionary until the user decides to perform a search. This provides flexibility in allowing the user to make, change, or discard selections before actually processing them.
+<br>
+`st.session_state['temp_selections'] = {}`
